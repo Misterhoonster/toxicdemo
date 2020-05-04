ad59ec9d9372c2ba40c46e0b1a1d585e64da7d2d
Misterhoonster <misterhoonster@gmail.com>
2020-05-03 23:53:06 -0500

    added preprocessing

---
+++ emojisplit.js
@@ -0,0 +1,91 @@
+var EmojiSplit = function(){
+	var json = '';
+	var predic = new Array(); 
+	var stack = '';
+	
+	this.loadData = function(){
+		json = (function () {
+		json = null;
+		$.ajax({
+			'async': false,
+			'global': false,
+			'url': 'https://raw.githubusercontent.com/thanhdnh/emoji-splitter/master/emoji.json',
+			'dataType': "json",
+			'success': function (data) {
+				json = data;
+			}
+		});
+		return json;
+		})(); 
+	}
+	
+	this.create = function(){
+		this.loadData();
+		for(var i=0; i<5; i++) predic[i] = new Array();
+		return this;
+	}
+
+	function decodeEntities(s){
+		var str, temp = document.createElement('p');
+		temp.innerHTML= s;
+		str= temp.textContent || temp.innerText;
+		temp=null;
+		return str;
+	}
+	
+	function generateUID() {
+	  function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+		  .toString(16)
+		  .substring(1);
+	  }
+	  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+		s4() + '-' + s4() + s4() + s4();
+	}
+	
+	function getBackFromUID(eleID, st){
+		return st[eleID];
+	}
+
+	this.splitBySymbol = function(emojistr, symbol){
+		if(symbol==undefined)
+			symbol = '/////';
+		$.each(json, function(k, v){
+			if(v['code_decimal'].split('&zwj;').length===4 || v['code_decimal'].split('&#8205;').length===4)
+				predic[4].push([decodeEntities(v['code_decimal']), v['unicode']]);
+			else if(v['code_decimal'].split('&zwj;').length===3 || v['code_decimal'].split('&#8205;').length===3)
+				predic[3].push([decodeEntities(v['code_decimal']), v['unicode']]);
+			else if(v['code_decimal'].split('&zwj;').length===2 || v['code_decimal'].split('&#8205;').length===2)
+				predic[2].push([decodeEntities(v['code_decimal']), v['unicode']]);
+			else if(v['code_decimal'].indexOf('&#127995')>0 || v['code_decimal'].indexOf('&#127996')>0 || v['code_decimal'].indexOf('&#127997')>0 || v['code_decimal'].indexOf('&#127998')>0 || v['code_decimal'].indexOf('&#127999')>0)
+				predic[1].push([decodeEntities(v['code_decimal']), v['unicode']]);
+			else
+				predic[0].push([decodeEntities(v['code_decimal']), v['unicode']]);
+		});
+		
+		stack = "{";		
+		for(var i=4; i>=0; i--)
+			$.each(predic[i], function(it, val){
+				if(emojistr.indexOf(val[0])>=0){
+					var guid = generateUID();
+					stack += '"'+guid+'":' + '"'+val[0]+'",';
+					emojistr = emojistr.replace(RegExp(val[0], 'g'), symbol+guid+symbol);
+				}
+			});
+		if(stack.length>2)
+			stack = stack.substring(0, stack.length-1)+'}';
+		else
+			stack = stack+'}';
+		var stackUID = JSON.parse(stack);
+			
+		var splitUID = emojistr.split(RegExp(symbol, 'g'));
+		var result = new Array();
+		for(var i=0; i<splitUID.length; i++){
+			if(stackUID[splitUID[i]]!=null)
+				result.push(stackUID[splitUID[i]]);
+			else
+				result.push(splitUID[i]);
+		}
+		return result.filter(Boolean);
+	}
+}
--- main.js
+++ main.js
@@ -1,4 +1,66 @@
-//model = await tf.loadLayersModel('model/model.json')
+
+//import * as tf from '@tensorflow/tfjs';
+//model = await tf.loadLayersModel('model/model.json');
+
+const url = 'https://api.npoint.io/6fa50cbaa66572bd0a81'
+
+let req = new XMLHttpRequest();
+req.open("GET", url, false);
+req.send(null);
+let wordtoindex = JSON.parse(req.responseText);
+
+//Credit: https://github.com/tensorflow/tfjs-examples
+function padSequences(
+sequences) {
+return sequences.map(seq => {
+  // Perform padding.
+  if (seq.length < 100) {
+    const pad = [];
+    for (let i = 0; i < 100 - seq.length; ++i) {
+      pad.push(0);
+    }
+    seq = pad.concat(seq);
+  }
+
+  return seq;
+});
+}
+
+function predict(text) {
+  let emojisplit = new EmojiSplit().create();
+  // Convert to lower case and remove all punctuations.
+  let rawText =
+      text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '');
+  rawText = emojisplit.splitBySymbol(rawText);
+  rawText = rawText.filter(v=>v.trim()!='');
+  console.log(rawText);
+  let inputText = []
+  for (let i = 0; i < rawText.length; i++) {
+    let text = rawText[i].split(' ');
+    text = text.filter(v=>v.trim()!='');
+    for (let j = 0; j < text.length; j++) {
+        inputText.push(text[j]);
+    }
+  }
+  // Convert the words to a sequence of word indices.
+  const sequence = inputText.map(word => {
+    if (wordtoindex[word] != null) {
+      let wordIndex = wordtoindex[word];
+      return wordIndex
+    }
+    else {
+      let wordIndex = 0;
+      return wordIndex
+    }
+  });
+  // Perform truncation and padding.
+  const paddedSequence = padSequences([sequence]);
+  //const input = tf.tensor(paddedSequence, [100]);
+
+  //const prediction = model.predict(input);
+  
+  return paddedSequence
+}
 
 $(function() {
     // Initializes and creates emoji set from sprite sheet
@@ -16,5 +78,5 @@ $(function() {
   document.querySelector('#button')
       .addEventListener('click', (e) => {
         const text = document.querySelector('#textbox').value;
-        console.log(text);
+        console.log(predict(text));
       });
